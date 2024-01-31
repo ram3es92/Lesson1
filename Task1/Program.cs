15547fa7e44b481766bff779aacdcf10d67e2039
Roman_Knyazev <ram3es92@mail.ru>
2024-01-31 22:22:19 +0300

    добавлены несколько переменных

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World - еще раз");
+﻿//Console.WriteLine("Hello, World!");
+
+int lalio = 100;
+Console.WriteLine(lalio);
+
+double d = 3.3;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
