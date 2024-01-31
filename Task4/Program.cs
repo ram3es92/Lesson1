a20848cd8d8837b267e61bdcaeed866740c860d9
Roman_Knyazev <ram3es92@mail.ru>
2024-01-31 23:07:00 +0300

    Tam ece zadanie 5

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,28 +1,39 @@
-﻿int a = 6;
-int b = 7;
-int c = 9;
-int d = 4;
-int e = 10;
+﻿// int a = 6;
+// int b = 7;
+// int c = 9;
+// int d = 4;
+// int e = 10;
 
-int max = a;
+// int max = a;
 
-if (b > max)
+// if (b > max)
+// {
+//     max = b;
+// }
+
+// if (c > max)
+// {
+//     max = c;
+// }
+
+// if (d > max)
+// {
+//     max = d;
+// }
+
+// if (e > max)
+// {
+//     max = e;
+// }
+// Console.WriteLine(max);
+
+int n = 100;
+int i = 1;
+
+while (i <= n)
 {
-    max = b;
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(" ");
+    i = i + 1;
 }
-
-if (c > max)
-{
-    max = c;
-}
-
-if (d > max)
-{
-    max = d;
-}
-
-if (e > max)
-{
-    max = e;
-}
-Console.WriteLine(max);
